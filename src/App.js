9b131cfb4e2c9f593fe63d47b256c233eae5fd0d
oneIwant <jsanchez0937@gmail.com>
2022-09-03 11:59:14 -0400

    updated api functionality and routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { getDataApi } from './network-requests';
+import {Routes, Route} from 'react-router-dom'
+import LandingPage from './components/LandingPage';
 
 function App() {
   const [company, setCompany] = useState(null)
   //run this only when component first hits the page
   useEffect(()=> {
-    getDataApi()
+    getDataApi("company/2")
       .then(res => setCompany(res))
   }, [])
   return (
     <div className="App">
-    {company?.name} {company?.headquarters}
+      {company?.name} {company?.headquarters}
+      <Routes>
+        <Route path="/" element = {<LandingPage/>}/>
+      </Routes>
     </div>
   );
 }
