6320245eabc891323099b5a5840c88d1ad767670
Novoo94 <Odareyes2@gmail.com>
2022-09-09 23:25:00 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { getDataApi } from './network-requests';
+import{Routes,Route} from 'react-router-dom'
+import LandingPage from './components/LandingPage';
 
 function App() {
   const [company, setCompany] = useState(null)
@@ -12,6 +14,9 @@ function App() {
   return (
     <div className="App">
     {company?.name} {company?.headquarters}
+    <Routes>
+      <Route path= "/" element={<LandingPage/>}/>
+    </Routes>
     </div>
   );
 }
