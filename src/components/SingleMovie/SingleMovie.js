8c23b3a6d9333481eb38a24f825d8921ee99a9d6
ManiG21 <manigrant023@gmail.com>
2022-09-17 10:32:05 -0400

    Additional Movie Tabs

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -1,32 +1,34 @@
 import './SingleMovie.css';
 import { useEffect, useState } from 'react';
 import { getDataApi, MovieImage } from '../../network-requests';
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import logo2 from './Logo2.gif';
-export default function SingleMovie(props){
-    const [movie, setMovie] = useState(props.movie)
-    console.log(movie)
+export default function SingleMovie(props) {
+  const [movie, setMovie] = useState(props.movie)
+  console.log(movie)
   //run this only when component first hits the page
-  let {id} = useParams();
-  useEffect(()=> {
+  let { id } = useParams();
+  useEffect(() => {
     if (movie) return
     //Get movie with onclick?
-    
+
     getDataApi(`movie/${id}`)
       .then(res => setMovie(res))
   }, [])
-    return(
-    <div> 
+  return (
+    <div>
       {/* <img id='logo2' src={logo2}></img> */}
-        <h2>{movie?.title}</h2>
-        <h1>{movie?.runtime} minutes</h1>
-        <h1>Rating: {movie?.vote_average} Out of 10 </h1>
-        <h1>Release Date: {movie?.release_date}</h1>
-       <h1> <a>Add To My List</a> </h1>
-        <MovieImage movie = {movie} />
-        <p>{movie?.overview}</p>
+      <h2>{movie?.title}</h2>
+      <h1>{movie?.runtime} minutes</h1>
+      <h1>Rating: {movie?.vote_average} Out of 10 </h1>
+      <h1>Release Date: {movie?.release_date}</h1>
+      <h1> <a>Play Trailer</a></h1>
+      <h1> <a>Play Movie</a></h1>
+      <h1> <a>Add To My List</a> </h1>
+      <MovieImage movie={movie} />
+      <p>{movie?.overview}</p>
     </div>
-    )
+  )
 }
 //Add to My List
 // {moviesList.map(movie => <Singlemovie />)}
