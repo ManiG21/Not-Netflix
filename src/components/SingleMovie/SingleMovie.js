5d8e3c14a1138efe1b1149b552af67c507ecf9d3
ManiG21 <manigrant023@gmail.com>
2023-10-01 15:09:48 -0400

    Prsenatation

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -2,7 +2,7 @@ import './SingleMovie.css';
 import { useEffect, useState } from 'react';
 import { getDataApi, MovieImage } from '../../network-requests';
 import { useParams } from "react-router-dom";
-import logo2 from './Logo2.gif';
+// import logo2 from './Logo2.gif';
 
 export default function SingleMovie(props) {
   const [movie, setMovie] = useState(props.movie)
@@ -20,9 +20,8 @@ export default function SingleMovie(props) {
       .then(res => setMovie(res))
   }, [])
   useEffect(() => {
-    // if (videos) return
-    //Get movie with onclick?
 
+    //fliter for movie trailer from youtube
     getDataApi(`movie/${id}/videos`)
       .then(res => setVideo(res.results.filter(movie => movie.name.toLowerCase().includes('trailer'))[0]))
   }, [])
@@ -37,21 +36,21 @@ export default function SingleMovie(props) {
         <h1>Release Date: {movie?.release_date}</h1>
         <h1> <a>Play Movie</a></h1>
         <h1> <a>Add To My List</a> </h1>
-        <p>Overview: <br/>  <br/>
-        {movie?.overview}</p>
+        <p>Overview: <br />  <br />
+          {movie?.overview}</p>
       </div>
-     {video.key?
-      <iframe
+      {video.key ?
+        <iframe
 
-        className='movie-trailer'
-        width="680" height="500"
-        frameBorder="0"
-        // src={`https://www.youtube.com/embed/${movieTrailerId}?enablejsapi=1`}
-        
-        src={`https://www.youtube.com/embed/${video.key}?enablejsapi=1`}
-        // src={`https://www.youtube.com/results?search_query=${id}?enablejsapi=1`}
-        allowFullScreen
-></iframe>:null}
+          className='movie-trailer'
+          width="680" height="500"
+          frameBorder="0"
+          // src={`https://www.youtube.com/embed/${movieTrailerId}?enablejsapi=1`}
+
+          src={`https://www.youtube.com/embed/${video.key}?enablejsapi=1`}
+          // src={`https://www.youtube.com/results?search_query=${id}?enablejsapi=1`}
+          allowFullScreen
+        ></iframe> : null}
 
       <MovieImage movie={movie} />
     </div>
